07e26b2963f7ae402375a0ab2b3504f5bc77bf88
nation-cabs <technicalsupport@nationcabs.co.za>
2026-07-09 12:14:35 +0200

    Added a responsive hambager button to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import "../styles/Navbar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/Navbar.css";
 import logo from "../assets/img6.jpeg";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
-     <div className="logo">
+      <div className="logo">
 
     <Link to="/" className="logo-link">
 
@@ -24,12 +29,51 @@ function Navbar() {
 
 </div>
 
-      <ul className="nav-links">
-    <li><a href="#home">Home</a></li>
-    <li><a href="#services">Services</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#contact">Contact</a></li>
-</ul>
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+
+        <li>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/services" onClick={() => setMenuOpen(false)}>
+            Services
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
+        </li>
+
+        <div className="mobile-buttons">
+          <Link
+            to="/login"
+            className="login-btn"
+            onClick={() => setMenuOpen(false)}
+          >
+            Login
+          </Link>
+
+          <Link
+            to="/signup"
+            className="register-btn"
+            onClick={() => setMenuOpen(false)}
+          >
+            Register
+          </Link>
+        </div>
+
+      </ul>
 
       <div className="nav-buttons">
         <Link to="/login" className="login-btn">
@@ -39,6 +83,13 @@ function Navbar() {
         <Link to="/signup" className="register-btn">
           Register
         </Link>
+      </div>
+
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
 
     </nav>
